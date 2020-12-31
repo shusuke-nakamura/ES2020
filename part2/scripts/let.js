8c0e1285be3bd750a85ca2e419fd5defe21b489c
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-01-01 08:53:54 +0900

    速習 ECMAScript2020の学習開始

---
+++ part2/scripts/let.js
@@ -0,0 +1,5 @@
+if (true) {
+  // var i = 1;
+  let i = 1; // ブロックのみ有効な変数
+}
+console.log(i);
---
+++ part2/scripts/let2.js
@@ -0,0 +1,8 @@
+switch (i) {
+  case 0:
+    let value = 'i=0';
+    break;
+  case 1:
+    let value = 'i=1';
+    break;
+}
